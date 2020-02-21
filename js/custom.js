79161885407497604fdcc215539517864ae461ce
Hugo <hugo010911@gmail.com>
2020-02-21 18:45:48 +0800

    commit

--- js/custom.js
+++ js/custom.js
@@ -20,189 +20,192 @@
 */
 
 
-	jQuery(function($) {
+jQuery(function ($) {
 	"use strict";
 
 
-	 /* ----------------------------------------------------------- */
-	 /*  Style Switcher
-	 /* ----------------------------------------------------------- */
+	/* ----------------------------------------------------------- */
+	/*  Style Switcher
+	/* ----------------------------------------------------------- */
 
-		(function($) { "use strict";
-			 $(document).ready(function(){
-					 $('.style-switch-button').click(function(){
-					 $('.style-switch-wrapper').toggleClass('active');
-					 });
-					 $('a.close-styler').click(function(){
-					 $('.style-switch-wrapper').removeClass('active');
-					 });
+	(function ($) {
+		"use strict";
+		$(document).ready(function () {
+			$('.style-switch-button').click(function () {
+				$('.style-switch-wrapper').toggleClass('active');
 			});
-		})(jQuery);
+			$('a.close-styler').click(function () {
+				$('.style-switch-wrapper').removeClass('active');
+			});
+			$('.dropdown-submenu').click(function (e) {
+				e.stopPropagation();
+				$(this).toggleClass('open');
+				$(this).siblings('.open').removeClass('open');
+			});
+		});
+	})(jQuery);
 
 
-	 /* ----------------------------------------------------------- */
-	 /*  Fixed header
-	 /* ----------------------------------------------------------- */
+	/* ----------------------------------------------------------- */
+	/*  Fixed header
+	/* ----------------------------------------------------------- */
 
-		(function() {
-				
-				var docElem = document.documentElement,
-					didScroll = false,
-					changeHeaderOn = 100;
-					document.querySelector( 'header' );
-					
-				function init() {
-					window.addEventListener( 'scroll', function() {
-						if( !didScroll ) {
-							didScroll = true;
-							setTimeout( scrollPage, 250 );
-						}
-					}, false );
+	(function () {
+
+		var docElem = document.documentElement,
+			didScroll = false,
+			changeHeaderOn = 100;
+		document.querySelector('header');
+
+		function init() {
+			window.addEventListener('scroll', function () {
+				if (!didScroll) {
+					didScroll = true;
+					setTimeout(scrollPage, 250);
 				}
-				
-				function scrollPage() {
-					var sy = scrollY();
-					if ( sy >= changeHeaderOn ) {
-						$('.top-bar').slideUp(300);
-						$("header").addClass("header-fixed");
-						
-					}
-					else {
-						$('.top-bar').slideDown(300);
-						$("header").removeClass("header-fixed");
-						
-					}
-					didScroll = false;
-				}
-				
-				function scrollY() {
-					return window.pageYOffset || docElem.scrollTop;
-				}
-				
-				init();
-				
-		})();
-
-
- 		/* ----------------------------------------------------------- */
-		/*  Parallax
-		/* ----------------------------------------------------------- */
-		function isScrolledIntoView(elem) {
-				var docViewTop = $(window).scrollTop();
-				var docViewBottom = docViewTop + $(window).height();
-				var elemTop = $(elem).offset().top;
-				return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+			}, false);
 		}
 
-		$.fn.parallax = function (options) {
+		function scrollPage() {
+			var sy = scrollY();
+			if (sy >= changeHeaderOn) {
+				$('.top-bar').slideUp(300);
+				$("header").addClass("header-fixed");
 
-				var windowHeight = $(window).height();
+			}
+			else {
+				$('.top-bar').slideDown(300);
+				$("header").removeClass("header-fixed");
 
-				// Establish default settings
-				var settings = $.extend({
-						speed  : 0.15,
-						type   : 'background',  // background, transform,
-				}, options);
-
-				// Iterate over each object in collection
-				return this.each(function () {
-
-						// Save a reference to the element
-						var $this = $(this);
-
-						// Set up Scroll Handler
-						$(window).on('scroll',function (e) {
-
-								//e.stopImmediatePropagation();
-
-								var scrollTop = $(window).scrollTop();
-								var offset = $this.offset().top;
-								var height = $this.outerHeight();
-
-								// Check if above or below viewport
-								if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
-										return;
-								}
-
-								var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
-
-								//console.log( yBgPosition );
-
-								var transform = Math.round((scrollTop) * settings.speed);
-
-
-								// Apply the Y Background Position to Set the Parallax Effect
-								if (settings.type == 'background') {
-										$this.css('background-position', 'center ' + -yBgPosition + 'px');
-								}
-
-								else if ((settings.type == 'transform')) {
-										$this.css('-webkit-transform', 'translateY(' + transform + 'px)')
-												.css('-moz-transform', 'translateY(' + transform + 'px)')
-												.css('transform', 'translateY(' + transform + 'px)');
-								}
-						});
-				});
+			}
+			didScroll = false;
 		}
 
-		$('#slideshow-wrapper').parallax({
-				speed: 0.5,
-				type : 'transform'
+		function scrollY() {
+			return window.pageYOffset || docElem.scrollTop;
+		}
+
+		init();
+
+	})();
+
+
+	/* ----------------------------------------------------------- */
+	/*  Parallax
+	/* ----------------------------------------------------------- */
+	function isScrolledIntoView(elem) {
+		var docViewTop = $(window).scrollTop();
+		var docViewBottom = docViewTop + $(window).height();
+		var elemTop = $(elem).offset().top;
+		return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+	}
+
+	$.fn.parallax = function (options) {
+
+		var windowHeight = $(window).height();
+
+		// Establish default settings
+		var settings = $.extend({
+			speed: 0.15,
+			type: 'background',  // background, transform,
+		}, options);
+
+		// Iterate over each object in collection
+		return this.each(function () {
+
+			// Save a reference to the element
+			var $this = $(this);
+
+			// Set up Scroll Handler
+			$(window).on('scroll', function (e) {
+
+				//e.stopImmediatePropagation();
+
+				var scrollTop = $(window).scrollTop();
+				var offset = $this.offset().top;
+				var height = $this.outerHeight();
+
+				// Check if above or below viewport
+				if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+					return;
+				}
+
+				var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+
+				//console.log( yBgPosition );
+
+				var transform = Math.round((scrollTop) * settings.speed);
+
+
+				// Apply the Y Background Position to Set the Parallax Effect
+				if (settings.type == 'background') {
+					$this.css('background-position', 'center ' + -yBgPosition + 'px');
+				}
+
+				else if ((settings.type == 'transform')) {
+					$this.css('-webkit-transform', 'translateY(' + transform + 'px)')
+						.css('-moz-transform', 'translateY(' + transform + 'px)')
+						.css('transform', 'translateY(' + transform + 'px)');
+				}
+			});
+		});
+	}
+
+	$('#slideshow-wrapper').parallax({
+		speed: 0.5,
+		type: 'transform'
+	});
+
+
+
+	/* ----------------------------------------------------------- */
+	/*  Parallax for Chrome
+	/* -----------------------------------------------------------*/
+
+	var isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+	if (isChrome) {
+
+		var itemArr = $('.parallax');
+
+		$(window).scroll(function () {
+			var pos = $(window).scrollTop();
+			var wh = window.innerHeight;
+
+			$(itemArr).each(function (i, item) {
+
+				var p = $(item).position();
+				var h = $(item).height();
+				if (p.top + h > pos && p.top < pos + wh) {
+					// items ir redzams
+					var prc = (p.top - pos + h) / wh;
+					//console.log(prc);
+					$(item).css({ 'background-position': 'center ' + prc + '%' });
+				}
+
+			});
 		});
 
-		
-
-		/* ----------------------------------------------------------- */
-		/*  Parallax for Chrome
-		/* -----------------------------------------------------------*/
-
-		var isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-		if (isChrome)
-		{
-
-			var itemArr = $('.parallax');
-
-			$(window).scroll(function()
-			{
-					var pos = $(window).scrollTop();
-					var wh = window.innerHeight;
-
-					$(itemArr).each(function(i, item){
-
-							var p = $(item).position();
-							var h = $(item).height();
-							if (p.top + h > pos && p.top < pos+wh)
-							{
-									// items ir redzams
-									var prc = (p.top - pos +h)/wh ;
-									//console.log(prc);
-									$(item).css({'background-position':'center '+prc+'%'});
-							}
-
-					});
-			});
-
-		}
+	}
 
 
 	/* ----------------------------------------------------------- */
 	/*  Main slideshow
 	/* ----------------------------------------------------------- */
 
-		 $('#main-slide').carousel({
-				pause: true,
-				interval: 100000,
-		 });
+	$('#main-slide').carousel({
+		pause: true,
+		interval: 100000,
+	});
 
 
-	 /* ----------------------------------------------------------- */
-	 /*  Counter
-	 /* ----------------------------------------------------------- */
+	/* ----------------------------------------------------------- */
+	/*  Counter
+	/* ----------------------------------------------------------- */
 
-		$('.counter').counterUp({
-		 delay: 10,
-		 time: 1000
-		});
+	$('.counter').counterUp({
+		delay: 10,
+		time: 1000
+	});
 
 
 
@@ -210,147 +213,147 @@
 	/*  Owl Carousel
 	/* ----------------------------------------------------------- */
 
-		//Testimonial
-		$("#testimonial-carousel").owlCarousel({
- 
-			navigation : true, // Show next and prev buttons
-			slideSpeed : 600,
-			pagination:false,
-			singleItem:true
- 
+	//Testimonial
+	$("#testimonial-carousel").owlCarousel({
+
+		navigation: true, // Show next and prev buttons
+		slideSpeed: 600,
+		pagination: false,
+		singleItem: true
+
+	});
+
+	// Custom Navigation Events
+	var owl = $("#testimonial-carousel");
+
+	// Custom Navigation Events
+	$(".next").click(function () {
+		owl.trigger('owl.next');
+	})
+	$(".prev").click(function () {
+		owl.trigger('owl.prev');
+	})
+	$(".play").click(function () {
+		owl.trigger('owl.play', 1000); //owl.play event accept autoPlay speed as second parameter
+	})
+	$(".stop").click(function () {
+		owl.trigger('owl.stop');
+	})
+
+
+	//Clients
+
+	$("#client-carousel").owlCarousel({
+
+		navigation: false, // Show next and prev buttons
+		slideSpeed: 400,
+		pagination: true,
+		items: 5,
+		rewindNav: true,
+		itemsDesktop: [1199, 3],
+		itemsDesktopSmall: [979, 3],
+		stopOnHover: true,
+		autoPlay: true
+
+	});
+
+	//Attorneys
+	$(".attorneys").owlCarousel({
+
+		navigation: true, // Show next and prev buttons
+		navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+		slideSpeed: 800,
+		pagination: false,
+		items: 4,
+		rewindNav: true,
+		itemsDesktop: [1199, 3],
+		itemsDesktopSmall: [979, 3],
+		stopOnHover: true
+
+	});
+
+	//Partners
+	$(".partners").owlCarousel({
+
+		navigation: true, // Show next and prev buttons
+		navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+		slideSpeed: 800,
+		pagination: false,
+		items: 4,
+		rewindNav: true,
+		itemsDesktop: [1199, 3],
+		itemsDesktopSmall: [979, 3],
+		stopOnHover: true
+
+	});
+
+	//Slide
+	$("#slide-carousel").owlCarousel({
+
+		navigation: true, // Show next and prev buttons
+		navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+		slideSpeed: 800,
+		pagination: false,
+		items: 3,
+		rewindNav: true,
+		itemsDesktop: [1199, 3],
+		itemsDesktopSmall: [979, 2],
+		stopOnHover: true
+
+	});
+
+
+	/* ----------------------------------------------------------- */
+	/*  Flex slider
+	/* ----------------------------------------------------------- */
+
+	//Second item slider
+	$(window).load(function () {
+		$('.flexSlideshow').flexslider({
+			animation: "fade",
+			controlNav: false,
+			directionNav: true,
+			slideshowSpeed: 8000
 		});
-
-		// Custom Navigation Events
-		var owl = $("#testimonial-carousel");
-
-		// Custom Navigation Events
-		$(".next").click(function(){
-			owl.trigger('owl.next');
-		})
-		$(".prev").click(function(){
-			owl.trigger('owl.prev');
-		})
-		$(".play").click(function(){
-			owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-		})
-		$(".stop").click(function(){
-			owl.trigger('owl.stop');
-		})
-		
-
-	 	//Clients
-
-		$("#client-carousel").owlCarousel({
-
-			navigation : false, // Show next and prev buttons
-			slideSpeed : 400,
-			pagination: true,
-			items : 5,
-			rewindNav: true,
-			itemsDesktop : [1199,3],
-			itemsDesktopSmall : [979,3],
-			stopOnHover:true,
-			autoPlay:true
-
-		});
-
-		//Attorneys
-		$(".attorneys").owlCarousel({
-
-			navigation : true, // Show next and prev buttons
-			navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-			slideSpeed : 800,
-			pagination:false,
-			items : 4,
-			rewindNav: true,
-			itemsDesktop : [1199,3],
-			itemsDesktopSmall : [979,3],
-			stopOnHover:true
-
-		});
-
-		//Partners
-		$(".partners").owlCarousel({
-
-			navigation : true, // Show next and prev buttons
-			navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-			slideSpeed : 800,
-			pagination:false,
-			items : 4,
-			rewindNav: true,
-			itemsDesktop : [1199,3],
-			itemsDesktopSmall : [979,3],
-			stopOnHover:true
-
-		});
-
-		//Slide
-		 $("#slide-carousel").owlCarousel({
-
-			 navigation : true, // Show next and prev buttons
-			 navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-			 slideSpeed : 800,
-			 pagination:false,
-			 items : 3,
-			 rewindNav: true,
-			 itemsDesktop : [1199,3],
-			 itemsDesktopSmall : [979,2],
-			 stopOnHover:true
-
-		 });
+		setTimeout(function () { $('#mask').fadeOut(500); }, 1000);
+	});
 
 
-	 /* ----------------------------------------------------------- */
-	 /*  Flex slider
-	 /* ----------------------------------------------------------- */
+	/* ----------------------------------------------------------- */
+	/*  Animation
+	/* ----------------------------------------------------------- */
 
-		//Second item slider
-		$(window).load(function() {
-			$('.flexSlideshow').flexslider({
-				 animation: "fade",
-				 controlNav: false,
-				 directionNav: true ,
-				 slideshowSpeed: 8000
-			});
-			setTimeout(function(){ $('#mask').fadeOut(500); }, 1000);
-		});
-
-		 
-	 /* ----------------------------------------------------------- */
-	 /*  Animation
-	 /* ----------------------------------------------------------- */
-
-		//Wow
-		new WOW().init();
+	//Wow
+	new WOW().init();
 
 
-	 /* ----------------------------------------------------------- */
-	 /*  Prettyphoto
-	 /* ----------------------------------------------------------- */
+	/* ----------------------------------------------------------- */
+	/*  Prettyphoto
+	/* ----------------------------------------------------------- */
 
-		$("a[data-rel^='prettyPhoto']").prettyPhoto();
+	$("a[data-rel^='prettyPhoto']").prettyPhoto();
 
 
-	 /* ----------------------------------------------------------- */
-	 /*  Back to top
-	 /* ----------------------------------------------------------- */
+	/* ----------------------------------------------------------- */
+	/*  Back to top
+	/* ----------------------------------------------------------- */
 
-			 $(window).scroll(function () {
-						if ($(this).scrollTop() > 50) {
-								$('#back-to-top').fadeIn();
-						} else {
-								$('#back-to-top').fadeOut();
-						}
-				});
-			// scroll body to 0px on click
-			$('#back-to-top').click(function () {
-					$('#back-to-top').tooltip('hide');
-					$('body,html').animate({
-							scrollTop: 0
-					}, 800);
-					return false;
-			});
-			
-			$('#back-to-top').tooltip('hide');
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) {
+			$('#back-to-top').fadeIn();
+		} else {
+			$('#back-to-top').fadeOut();
+		}
+	});
+	// scroll body to 0px on click
+	$('#back-to-top').click(function () {
+		$('#back-to-top').tooltip('hide');
+		$('body,html').animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+
+	$('#back-to-top').tooltip('hide');
 
 });
